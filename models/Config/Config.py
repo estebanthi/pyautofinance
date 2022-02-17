5d162e55d43365ad95bbc0c98c5f7822ae4f71af
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-02-17 23:03:38 +0100

    config field

--- models/Config/Config.py
+++ models/Config/Config.py
@@ -8,8 +8,11 @@ class Config:
         config_loader = ConfigLoader("config.yml")
         self.config = config_loader.get_config()
 
+    def get_field(self, field):
+        field_value = self.config.get(field)
+        if not field_value:
+            raise ConfigFieldMissing(field)
+        return field_value
+
     def get_datasets_pathname(self):
-        datasets_path = self.config.get("datasets_pathname")
-        if not datasets_path:
-            raise ConfigFieldMissing("datasets_pathname")
-        return datasets_path
+        return self.get_field("datasets_pathname")
