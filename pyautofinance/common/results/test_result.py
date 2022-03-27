fa35dddfdbc5bac3cbe7395d6795c86ecafbf707
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-03-27 19:14:06 +0200

    Update test_result.py

--- pyautofinance/common/results/test_result.py
+++ pyautofinance/common/results/test_result.py
@@ -3,9 +3,9 @@ from pyautofinance.common.metrics.metrics_collection import MetricsCollection
 
 class TestResult:
 
-    def __init__(self, *metrics):
+    def __init__(self, valid=False, *metrics):
+        self.valid = valid
         self.metrics = MetricsCollection(*metrics)
 
     def __getitem__(self, item):
         return self.metrics[item]
-    
