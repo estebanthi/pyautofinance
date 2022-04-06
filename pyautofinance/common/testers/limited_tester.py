9fa79d7be792b981ee6c519ae53026f949d05068
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-04-06 23:25:46 +0200

    Create limited_tester.py

---
+++ pyautofinance/common/testers/limited_tester.py
@@ -0,0 +1,16 @@
+from pyautofinance.common.testers.tester import Tester
+from pyautofinance.common.results.test_result import TestResult
+from pyautofinance.common.results.test_results_collection import TestResultsCollection
+
+
+class MonkeyTester(Tester):
+
+    def __init__(self, limited_strat):
+        self.limited_strat = limited_strat
+
+    def test(self, engine):
+        engine.components_assembly[1] = self.limited_strat
+        engine_result = engine.run()
+
+        test_results = [strat_result['Winrate'] for strat_result in engine_result]
+        return TestResultsCollection(*test_results)
