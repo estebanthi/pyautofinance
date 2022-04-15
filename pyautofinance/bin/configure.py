bce1d56bc74942f62f737510f664cf02dc2b4684
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-04-15 14:11:36 +0200

    Scripts

--- pyautofinance/bin/configure.py
+++ pyautofinance/bin/configure.py
@@ -1,9 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 from pyautofinance.common.config.configurator import Configurator
 
 
-configurators_paths = ['.']
+def run(configurators_paths):
+    for path in configurators_paths:
+        configurator = Configurator(path)
+        configurator.create_config_file()
+        configurator.create_folders()
 
-for path in configurators_paths:
-    configurator = Configurator(path)
-    configurator.create_config_file()
-    configurator.create_folders()
+
+if __name__ == '__main__':
+    configurators_paths = ['.']
+
+    run(configurators_paths)
--- pyautofinance/bin/data_extraction.py
+++ pyautofinance/bin/data_extraction.py
@@ -2,21 +2,15 @@ import datetime as dt
 import warnings
 warnings.filterwarnings('ignore')
 
-import pandas_ta as ta
-from sklearn.ensemble import RandomForestClassifier
-
 from pyautofinance.common.timeframes import h1
 from pyautofinance.common.dataflux import DiskDataflux
 from pyautofinance.common.feeds.extractors import CCXTCandlesExtractor
 from pyautofinance.common.feeds import BackDatafeed
-from pyautofinance.common.learn import TaLibPredicter
 
 
-def run(symbol, start_date, end_date, timeframe, dataflux, predicter, extractor=None):
+def run(symbol, start_date, end_date, timeframe, dataflux, extractor):
     feed = BackDatafeed(symbol, start_date, timeframe, end_date, dataflux, candles_extractor=extractor)
-
-    predicter.fit(feed)
-    predicter.save('BTC-EUR 1h')
+    feed.extract()
 
 if __name__ == '__main__':
     symbol = 'BTC-EUR'
@@ -26,8 +20,4 @@ if __name__ == '__main__':
     dataflux = DiskDataflux()
     extractor = CCXTCandlesExtractor()
 
-    model = RandomForestClassifier()
-    strategy = ta.AllStrategy
-    predicter = TaLibPredicter(model=model, ta_strategy=strategy)
-
-    run(symbol, start_date, end_date, timeframe, dataflux, predicter, extractor=extractor)
+    run(symbol, start_date, end_date, timeframe, dataflux, extractor)
